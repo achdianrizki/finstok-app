8e951688c6c05858b1a3b1faf41b5bb23d3b3600
rizalrandyy <rizalrandy3@gmail.com>
2024-09-10 11:04:50 +0700

    feat: gudang, comp: table

---
+++ resources/views/components/table/tbody.blade.php
@@ -0,0 +1,3 @@
+<tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-dark-eval-1">
+  {{ $slot }}
+</tbody>
---
+++ resources/views/manager/warehouse/create.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+  <x-slot name="header">
+      <div class="flex items-center gap-4">
+          <x-button target="" href="{{ route('manager.warehouse.index') }}" variant="primary" size="sm" class="justify-center gap-2">
+              <x-heroicon-o-arrow-left class="w-4 h-4" aria-hidden="true" />
+          </x-button>
+          <h2 class="text-xl font-semibold leading-tight">
+              {{ __('Tambah gudang') }}
+          </h2>
+      </div>
+  </x-slot>    
+
+  <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+      <form action="{{ route('manager.warehouse.store') }}" method="POST">
+          @csrf
+
+          <div class="grid gap-2">
+              <div class="mb-5 space-y-2">
+                  <x-form.label for="name" :value="__('Nama gudang')" />
+                  <x-form.input id="name" class="block w-full" type="text" name="name" :value="old('name')"
+                      placeholder="{{ __('Nama gudang') }}" required />
+              </div>
+
+              <div class="mb-5 space-y-2">
+                  <x-form.label for="address" :value="__('Alamat')" />
+                  <x-form.input id="address" class="block w-full" type="text" name="address" :value="old('address')"
+                      placeholder="{{ __('Alamat') }}" required />
+              </div>
+
+              <div class="grid justify-items-end">
+                  <x-button class="gap-2">
+                      <span>{{ __('Submit') }}</span>
+                  </x-button>
+              </div>
+
+          </div>
+      </form>
+  </div>
+
+</x-app-layout>
